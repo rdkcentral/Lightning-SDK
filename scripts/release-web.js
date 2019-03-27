12ef01b53699f7ac7ae210e372b6cf09246ccf46
basvanmeurs <bvanmeurs1985@gmail.com>
2019-03-27 14:55:46 +0100

    fix(release-web): new path to lightning-web.js

--- scripts/release-web.js
+++ scripts/release-web.js
@@ -65,7 +65,7 @@ function copyUxFiles() {
 }
 
 function copyLightning() {
-    return exec("cp -r " + dir + "/../wpe-lightning/dist/lightning-web.js ./dist/" + info.dest + "/js/src/");
+    return exec("cp -r " + dir + "/js/lib/lightning-web.js ./dist/" + info.dest + "/js/src/");
 }
 
 function copyAppFiles() {
