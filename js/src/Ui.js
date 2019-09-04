e20c123c920f53c3196e72103d9e3bc29d7d4bce
erikhaandrikman <e.haandrikman@metrological.com>
2019-09-04 15:33:43 +0200

    update(Ui): start app when loadFonts is fulfilled or rejected

--- js/src/Ui.js
+++ js/src/Ui.js
@@ -94,8 +94,9 @@ export default class Ui extends lng.Application {
                                     this._currentApp.fontFaces = fontFaces;
                                 }).catch((e) => {
                                     console.warn('Font loading issues: ' + e);
+                                }).finally(()=>{
+                                    this._done();
                                 });
-                                this._done();
                             }
                             _done() {
                                 this._setState("App.Started");
