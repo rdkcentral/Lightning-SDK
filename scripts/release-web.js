54c213fa4f640c478dad7ff8af3c5b00ccbc0b37
Grzegorz Zachar <g.zachar@metrological.com>
2019-11-06 16:49:58 +0100

    Fixed lightning filename.

--- scripts/release-web.js
+++ scripts/release-web.js
@@ -108,7 +108,7 @@ function ensureSrcDirs() {
 function babelify() {
     return Promise.all([
         babelifyFile("./dist/" + info.dest + "/js/src/appBundle.js", "./dist/" + info.dest + "/js/src.es5/appBundle.js"),
-        babelifyFile("./dist/" + info.dest + "/js/src/lightning-web.js", "./dist/" + info.dest + "/js/src.es5/lightning-web.js"),
+        babelifyFile("./dist/" + info.dest + "/js/src/lightning.js", "./dist/" + info.dest + "/js/src.es5/lightning.js"),
         babelifyFile("./dist/" + info.dest + "/js/src/ux.js", "./dist/" + info.dest + "/js/src.es5/ux.js")
     ])
 }
