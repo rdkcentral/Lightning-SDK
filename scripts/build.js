99f980e527f51da336256b19148f33e23a888e53
wouterlucas <wouter@wouterlucas.com>
2019-11-19 15:43:02 -0800

    Set appName in appBundle when doing rollup

--- scripts/build.js
+++ scripts/build.js
@@ -14,25 +14,56 @@ const inputOptions = {
     plugins: [resolve({ browser: true }), commonjs(), babel()],
 }
 
-const outputOptions = {
+let outputOptions = {
     format: 'iife',
-    name: 'appBundle',
+    name: 'APP_',
     file: path.join(baseDir, '/dist/appBundle.js'),
 }
 
-rollup(inputOptions)
-    .then(bundle => {
-    return bundle
-        .generate(outputOptions)
-        .then(() => {
-            bundle
-                .write(outputOptions)
-                .then(console.log('App bundle written to ' + outputOptions.file))
-                .catch(console.error)
+const getName = function() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(baseDir + "/metadata.json", function(err, res) {
+            if (err) {
+                console.error(err)
+                return reject(new Error("Metadata.json file can't be read: run this from a directory containing a metadata file."));
+            }
+
+            const contents = res.toString();
+            const data = JSON.parse(contents);
+
+            if (!data.identifier) {
+                return reject(new Error("Can't find identifier in metadata.json file"));
+            }
+
+            if(!data.version){
+                return reject(new Error("Can't find version in metadata.json file"));
+            }
+
+            if(!data.name){
+                return reject(new Error("No name provided for your app"));
+            }
+
+            return resolve(data);
+        });
+    });
+}
+
+getName().then( (data) => {
+    outputOptions.name = "APP_" + data.identifier.replace(/[^0-9a-zA-Z_$]/g, "_");
+    rollup(inputOptions)
+        .then(bundle => {
+        return bundle
+            .generate(outputOptions)
+            .then(() => {
+                bundle
+                    .write(outputOptions)
+                    .then(console.log('App bundle written to ' + outputOptions.file))
+                    .catch(console.error)
+            })
+            .catch(console.error)
         })
         .catch(console.error)
-    })
-    .catch(console.error)
+})
 
 
 fs.copyFile(
