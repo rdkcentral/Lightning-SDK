21d48f6a579440c515abef8118899b62430a16ec
Michiel van der Geest <hello@michielvandergeest.com>
2019-11-18 21:52:07 -0300

    Updated profile plugin.

--- src/Launch/index.js
+++ src/Launch/index.js
@@ -1,11 +1,13 @@
 import Utils from '../Utils'
+import { initProfile } from '../Profile'
 import Application from '../Application'
 
 export default (App, appSettings, platformSettings) => {
     
     Utils.setPath(platformSettings.path)
-    // todo
-    // platformSettings.plugins.metrics && Metrics.init(environment.plugins.metrics)
+
+    // Initialize plugin profile
+    platformSettings.plugins.profile && initProfile(platformSettings.plugins.profile)
     
     const app = Application(App)
     return new app(appSettings)
--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,76 +1,43 @@
-export default (config) => {
+let getInfo = () => Promise.resolve()
+let setInfo = () => Promise.resolve()
 
-    // perhaps do some intialization here based on config
+export const initProfile = (config) => {
+    getInfo = config.getInfo
+    setInfo = config.setInfo
+}
 
+const getOrSet = (key, params) => params ? setInfo(key, params) : getInfo(key)
 
-    // variable to store the profile info
-    let profile = null
-
-    // retrieve info via moneybadger
-    // (this should be made dynamic based on platform)
-    var retrieveInfo = () => {
-        return new Promise((resolve, reject) => {
-            
-            window.$badger.info().succes((data) => {
-                // maybe do some preformatting of data
-                // or some mapping (i.e. write key postcode to a uniform zipcode)
-                // ...
-                // store profile data for later use
-                profile = data
-                // resolve the data
-                resolve(data)
-            })
-            .failure(() => {
-                reject()
-            })
-        })
-    }
-
-    // get a specific piece of info
-    const getInfo = (key) => {
-        return new Promise((resolve, reject) => {
-            if(!profile) {
-                retrievInfo().then(data => {
-                    resolve(data[key])
-                })
-            }
-            else {
-                resolve(profile[key])
-            }
-        })   
-    }
-
-    // public API
-    return {
-        zipcode() {
-            return getInfo('zipcode')
-        },
-        timezone() {
-            return getInfo('timezone')
-        },
-        partnerId() {
-            return getInfo('partnerId')
-        },
-        receiverId() {
-            return getInfo('receiverId')
-        },
-        deviceId() {
-            return getInfo('deviceId')
-        },
-        deviceHash() {
-            return getInfo('deviceHash')
-        },
-        deviceCapabilities() {
-            return getInfo('deviceCapabilities')
-        },
-        householdId() {
-            return getInfo('householdId')
-        },
-        clientId() {
-            return getInfo('clientId')
-        },
-        locationId() {
-            return getInfo('locationId')
-        },
-    }       
+// public API
+export default {
+    zipcode(params) {
+        return getOrSet('zipcode', params)
+    },
+    timezone() {
+        return getOrSet('timezone', params)
+    },
+    partnerId() {
+        return getOrSet('partnerId', params)
+    },
+    receiverId() {
+        return getOrSet('receiverId', params)
+    },
+    deviceId() {
+        return getOrSet('deviceId', params)
+    },
+    deviceHash() {
+        return getOrSet('deviceHash', params)
+    },
+    deviceCapabilities() {
+        return getOrSet('deviceCapabilities', params)
+    },
+    householdId() {
+        return getOrSet('householdId', params)
+    },
+    clientId() {
+        return getOrSet('clinetId', params)
+    },
+    locationId() {
+        return getOrSet('locationId', params)
+    },     
 }
