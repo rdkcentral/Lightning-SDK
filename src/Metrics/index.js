1ef6268b2e5ff14ffd714c9f1b548d52c184b8ee
Michiel van der Geest <hello@michielvandergeest.com>
2019-11-25 11:12:48 -0300

    Updated metrics plugin to be more flexible and shorter (especially when more exposed metrics are added) - inspired by @wouterlucas.

--- src/Metrics/index.js
+++ src/Metrics/index.js
@@ -1,4 +1,3 @@
-// dummy implementation of send metric (should be a function in platform plugin, passed to initMetrics)
 let sendMetric = (type, event, params) => {
   console.log('Sending metric', { type }, { event }, { params })
 }
@@ -7,116 +6,45 @@ export const initMetrics = config => {
   sendMetric = config.sendMetric
 }
 
-// public API
-export default {
-  app: {
-    // called automatically when Application is launched (after plugins are initialized)
-    launch(params = {}) {
-      sendMetric('app', 'launch', params)
+// available metric per category
+const metrics = {
+  app: ['launch', 'loaded', 'ready', 'close'],
+  page: ['view', 'leave'],
+  user: ['click', 'input'],
+  media: ['abort', 'canplay', 'ended', 'pause', 'play', 'suspend', 'volumechange', 'waiting'],
+}
+
+// error metric function (added to each category)
+const errorMetric = (type, message, code, visible, params = {}) => {
+  params = { params, ...{ message, code, visible } }
+  sendMetric(type, 'error', params)
+}
+
+const Metric = (type, events) => {
+  return events.reduce(
+    (obj, event) => {
+      obj[event] = (params = {}) => sendMetric(type, event, params)
+      return obj
     },
-    // called automatically in Application when everything is loaded and App is attached
-    loaded(params = {}) {
-      sendMetric('app', 'loaded', params)
-    },
-    ready(params = {}) {
-      sendMetric('app', 'ready', params)
-    },
-    // can we do this automatically?
-    close(params = {}) {
-      sendMetric('app', 'close', params)
-    },
-    // todo: possibly make arguments like code and visible optional?
-    error(message, code, visible, params = {}) {
-      params.message = message
-      params.code = code
-      params.visible = visible
-      sendMetric('app', 'error', params)
-    },
-  },
-  page: {
-    view(name, params = {}) {
-      params.name = name
-      sendMetric('page', 'view', params)
-    },
-    leave(name, params = {}) {
-      params.name = name
-      sendMetric('page', 'leave', params)
-    },
-  },
-  user: {
-    click(name, params = {}) {
-      params.name = name
-      sendMetric('user', 'click', params)
-    },
-    input(name, params = {}) {
-      params.name = name
-      sendMetric('user', 'input', params)
-    },
-    // todo: possibly make arguments like code and visible optional?
-    error(message, code, visible, params = {}) {
-      params.message = message
-      params.code = code
-      params.visible = visible
-      sendMetric('user', 'error', params)
-    },
-  },
-  // not sure about this part yet ...
-  // these metrics should be integrated in the MediaPlayer class so they are called without each dev having to implement them
-  // (with option to DIY when not using standard MediaPlayer)
-  // example usage:
-  // const metrics = Metrics.media('http://metrological.com/video.mp4)
-  // metrics.
-  media(url) {
-    return {
-      abort(params = {}) {
-        params.url = url
-        sendMetric('media', 'abort', params)
+    {
+      error(message, code, params) {
+        errorMetric(type, message, code, params)
       },
-      canplay(params = {}) {
-        params.url = url
-        sendMetric('media', 'canplay', params)
-      },
-      ended(params = {}) {
-        params.url = url
-        sendMetric('media', 'ended', params)
-      },
-      pause(params = {}) {
-        params.url = url
-        sendMetric('media', 'pause', params)
-      },
-      play(params = {}) {
-        params.url = url
-        sendMetric('media', 'play', params)
-      },
-      suspend(params = {}) {
-        params.url = url
-        sendMetric('media', 'suspend', params)
-      },
-      volumechange(params = {}) {
-        params.url = url
-        sendMetric('media', 'volumechange', params)
-      },
-      waiting(params = {}) {
-        params.url = url
-        sendMetric('media', 'waiting', params)
-      },
-      error(error, params = {}) {
-        // error is a HTMLMediaElement error (https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/error)
-        params.message = error.message
-        params.code = error.code
-        params.visible = false
-        sendMetric('media', 'error', params)
+      event(name, params) {
+        sendMetric(type, name, params)
       },
     }
-  },
-  // possibly make arguments like code and visible optional?
-  error(type, message, code, visible, params = {}) {
-    params.message = message
-    params.code = code
-    params.visible = visible
-    sendMetric(type, 'error', params)
-  },
-  event(type, name, params) {
-    sendMetric(type, name, params)
-  },
+  )
 }
+
+const Metrics = types => {
+  return Object.keys(types).reduce(
+    (obj, type) => {
+      obj[type] = Metric(type, types[type])
+      return obj
+    },
+    { error: errorMetric, event: sendMetric }
+  )
+}
+
+export default Metrics(metrics)
