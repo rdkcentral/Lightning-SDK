f20ef169968f037037b770e657d96e0274fdbc67
wouterlucas <wouter@wouterlucas.com>
2019-11-26 12:56:49 -0800

    Improve package script

--- scripts/package.js
+++ scripts/package.js
@@ -4,8 +4,8 @@ import fs from 'fs'
 import path from 'path'
 import { rollup } from 'rollup'
 import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
 import process from 'process'
 
 const baseDir = process.env.npm_config_baseDir
@@ -13,23 +13,18 @@ const dest = path.join(baseDir, 'dist/')
 
 let logging = true
 
+const inputOptions = {
+  input: path.join(baseDir, 'src/index.js'),
+  plugins: [resolve({ browser: true }), commonjs(), babel()],
+}
+
 export const release = () => {
   return new Promise(resolve => {
-    return getName().then(data => {
-      return ensureDir().then(() => {
-        return Promise.all([
-          copyMetadata(),
-          copyAppFiles(),
-          copyAppSrc(),
-          bundleApp(data),
-          bundleAppEs5(data),
-        ])
-          .then(() => pack(data))
-          .then(() => {
-            data.absolutePath = `${baseDir}/${data.identifier}.tgz`
-            log('MPK file created! ', data.absolutePath)
-            resolve(data)
-          })
+    return build().then(data => {
+      pack(data).then(() => {
+        data.absolutePath = `${baseDir}/${data.identifier}.tgz`
+        log('MPK file created! ' + data.absolutePath)
+        resolve(data)
       })
     })
   })
@@ -37,11 +32,11 @@ export const release = () => {
 
 export const build = () => {
   return new Promise(resolve => {
-    return getName()
-      .then(data => {
+    return Promise.all([getName(), copyFiles()])
+      .then(res => {
+        let data = res[0]
         return Promise.all([copyStartApp(), bundleApp(data), bundleAppEs5(data)]).then(() => {
-          data.absolutePath = `${baseDir}/dist/${data.identifier}.tgz`
-          log('Files written to ' + data.absolutePath)
+          log('Files written to ' + dest)
           resolve(data)
         })
       })
@@ -60,8 +55,7 @@ function getName() {
         )
       }
 
-      const contents = res.toString()
-      const data = JSON.parse(contents)
+      const data = JSON.parse(res.toString())
 
       if (!data.identifier) {
         return reject(new Error("Can't find identifier in metadata.json file"))
@@ -80,79 +74,51 @@ function getName() {
   })
 }
 
-const ensureDir = () => {
-  return new Promise(resolve => {
-    if (fs.existsSync(dest)) shell.rm('-r', dest)
-
-    fs.mkdirSync(dest)
-
-    resolve()
-  })
-}
-
-const copyMetadata = () => {
-  return new Promise(resolve => {
-    fs.copyFile(path.join(baseDir, 'metadata.json'), dest, resolve)
-  })
-}
-
-const copyAppFiles = () => {
-  return new Promise(resolve => {
-    //note: shelljs is sync
-    shell.cp('-r', path.join(baseDir, 'static/'), dest)
-    resolve()
-  })
-}
-
-const copyAppSrc = () => {
-  return new Promise(resolve => {
-    shell.cp('-r', path.join(baseDir, './src'), dest)
-    resolve()
-  })
-}
-
 const bundleApp = data => {
-  let outputOptions = {
+  const outputOptionsEs6 = {
     format: 'iife',
     name: 'APP_' + data.identifier.replace(/[^0-9a-zA-Z_$]/g, '_'),
     file: path.join(baseDir, '/dist/appBundle.js'),
   }
 
-  let inputOptions = {
-    input: path.join(baseDir, 'src/index.js'),
-    plugins: [resolve({ browser: true }), commonjs(), babel()],
-  }
-
-  return _bundleApp(inputOptions, outputOptions)
+  return _bundleApp(inputOptions, outputOptionsEs6)
 }
 
 const bundleAppEs5 = data => {
-  let outputOptionsEs5 = {
+  let inputOptionsEs5 = inputOptions
+  // FICSME add es5 specific plugins to the generic list
+  //inputOptionsEs5.plugins.concat([..])
+
+  const outputOptionsEs5 = {
     format: 'iife',
     name: 'APP_' + data.identifier.replace(/[^0-9a-zA-Z_$]/g, '_'),
     file: path.join(baseDir, '/dist/appBundle_es5.js'),
   }
 
-  let inputOptionsEs5 = {
-    input: path.join(baseDir, 'src/index.js'),
-    plugins: [resolve({ browser: true }), commonjs(), babel()],
-  }
-
   return _bundleApp(inputOptionsEs5, outputOptionsEs5)
 }
 
-const _bundleApp = (inputOptions, outputOptions) => {
+const _bundleApp = (_inputOptions, _outputOptions) => {
   return new Promise(resolve => {
-    rollup(inputOptions)
+    rollup(_inputOptions)
       .then(bundle => {
-        bundle.generate(outputOptions).then(() => {
-          bundle.write(outputOptions).then(() => {
-            console.log('App bundle written to ' + outputOptions.file)
+        bundle.generate(_outputOptions).then(() => {
+          bundle.write(_outputOptions).then(() => {
+            console.log('App bundle written to ' + _outputOptions.file)
             resolve()
           })
         })
       })
       .catch(console.error)
+  })
+}
+
+const copyFiles = () => {
+  return new Promise(resolve => {
+    //note: shelljs is sync
+    shell.cp('-r', path.join(baseDir, 'static/'), dest)
+    shell.cp('-r', path.join(baseDir, './src'), dest)
+    fs.copyFile(path.join(baseDir, 'metadata.json'), dest, resolve)
   })
 }
 
@@ -173,7 +139,6 @@ const pack = data => {
 }
 
 const tar = (src, dest) => {
-  console.log('tar', src, dest)
   return new Promise((resolve, reject) => {
     targz.compress(
       {
@@ -185,7 +150,7 @@ const tar = (src, dest) => {
           log('ERR:', err)
           reject(err)
         } else {
-          log(`TAR: ${src}`)
+          //log(`TAR: ${src}`)
           resolve()
         }
       }
