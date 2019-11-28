e3ba258536ca138d147388577313052871f95063
Michiel van der Geest <hello@michielvandergeest.com>
2019-11-28 12:28:18 -0300

    Changed media metric into a function that accepts a url, which is passed as a param to every metric call.

--- src/Metrics/index.js
+++ src/Metrics/index.js
@@ -20,10 +20,13 @@ const errorMetric = (type, message, code, visible, params = {}) => {
   sendMetric(type, 'error', params)
 }
 
-const Metric = (type, events) => {
+const Metric = (type, events, options = {}) => {
   return events.reduce(
     (obj, event) => {
-      obj[event] = (params = {}) => sendMetric(type, event, params)
+      obj[event] = (params = {}) => {
+        params = { ...options, ...params }
+        sendMetric(type, event, params)
+      }
       return obj
     },
     {
@@ -40,7 +43,12 @@ const Metric = (type, events) => {
 const Metrics = types => {
   return Object.keys(types).reduce(
     (obj, type) => {
-      obj[type] = Metric(type, types[type])
+      // media metric works a bit different!
+      // it's a function that accepts a url and returns an object with the available metrics
+      // url is automatically passed as a param in every metric
+      type === 'media'
+        ? (obj[type] = url => Metric(type, types[type], { url }))
+        : (obj[type] = Metric(type, types[type]))
       return obj
     },
     { error: errorMetric, event: sendMetric }
