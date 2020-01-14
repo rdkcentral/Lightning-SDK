3ce322e06d0fd1789883eaaf49420c19a0a666e1
Michiel van der Geest <hello@michielvandergeest.com>
2020-01-14 11:05:26 -0300

    Fixed scope of apply() in Log plugin (fails in older es5 browsers).

--- src/Log/index.js
+++ src/Log/index.js
@@ -18,15 +18,15 @@ const prepLog = (type, args) => {
 
 export default {
   info() {
-    Settings.get('platform', 'log') && console.log.apply(null, prepLog('Info', arguments))
+    Settings.get('platform', 'log') && console.log.apply(console, prepLog('Info', arguments))
   },
   debug() {
-    Settings.get('platform', 'log') && console.debug.apply(null, prepLog('Debug', arguments))
+    Settings.get('platform', 'log') && console.debug.apply(console, prepLog('Debug', arguments))
   },
   error() {
-    Settings.get('platform', 'log') && console.error.apply(null, prepLog('Error', arguments))
+    Settings.get('platform', 'log') && console.error.apply(console, prepLog('Error', arguments))
   },
   warn() {
-    Settings.get('platform', 'log') && console.warn.apply(null, prepLog('Warn', arguments))
+    Settings.get('platform', 'log') && console.warn.apply(console, prepLog('Warn', arguments))
   },
 }
