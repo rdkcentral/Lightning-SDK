98f5f24ffa82f09cc84db569386bda3258386cae
Michiel van der Geest <hello@michielvandergeest.com>
2020-01-15 10:46:40 -0300

    Fixed paths to node_modules t work local and via npm install in postinstall script.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,6 +1,14 @@
 import shell from 'shelljs'
+import path from 'path'
+import process from 'process'
+
 const target = './support/lib'
+const origin = path.join(
+  process.cwd(),
+  process.cwd().indexOf('/node_modules/') > -1 ? '..' : 'node_modules',
+  'wpe-lightning'
+)
 
 shell.mkdir(target)
-shell.cp('-R', './node_modules/wpe-lightning/devtools/*', target)
-shell.cp('-R', './node_modules/wpe-lightning/dist/*', target)
+shell.cp('-R', origin + '/devtools/*', target)
+shell.cp('-R', origin + '/dist/*', target)
