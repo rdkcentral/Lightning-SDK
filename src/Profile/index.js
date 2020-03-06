5de7620322f1bdee8ec3ce0047e1a00c4594c9f3
Johan van Leest <j.van.leest@metrological.com>
2020-03-06 14:14:29 +0100

    code lint

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,66 +1,66 @@
-import Settings from '../Settings/index';
-import { initDefaults, defaults } from './defaults';
+import Settings from '../Settings/index'
+import { initDefaults, defaults } from './defaults'
 
-let getInfo = (key)=>{
-  const value = Settings.get('profile', key);
-  if(value) return value;
-  return defaults[key];
-};
+let getInfo = key => {
+  const value = Settings.get('profile', key)
+  if (value) return value
+  return defaults[key]
+}
 
-let setInfo = (key, params)=>{
-  if(key in defaults) defaults[key] = params;
-};
+let setInfo = (key, params) => {
+  if (key in defaults) defaults[key] = params
+}
 
-export const initProfile = config=>{
-  getInfo = config.getInfo;
-  setInfo = config.setInfo;
-  initDefaults();
-};
+export const initProfile = config => {
+  getInfo = config.getInfo
+  setInfo = config.setInfo
+  initDefaults()
+}
 
-const getOrSet = (key, params)=>(params ? setInfo(key, params) : getInfo(key));
+const getOrSet = (key, params) => (params ? setInfo(key, params) : getInfo(key))
 
 // public API
 export default {
-  ageRating(params){
-    return getOrSet('ageRating', params);
+  ageRating(params) {
+    return getOrSet('ageRating', params)
   },
-  city(params){
-    return getOrSet('city', params);
+  city(params) {
+    return getOrSet('city', params)
   },
-  countryCode(params){
-    return getOrSet('countryCode', params);
+  countryCode(params) {
+    return getOrSet('countryCode', params)
   },
-  ip(params){
-    return getOrSet('ip', params);
+  ip(params) {
+    return getOrSet('ip', params)
   },
-  household(params){
-    return getOrSet('household', params);
+  household(params) {
+    return getOrSet('household', params)
   },
-  language(params){
-    return getOrSet('language', params);
+  language(params) {
+    return getOrSet('language', params)
   },
-  latlon(params){
-    return getOrSet('latlon', params);
+  latlon(params) {
+    return getOrSet('latlon', params)
   },
-  locale(params){
-    return getOrSet('locale', params);
+  locale(params) {
+    return getOrSet('locale', params)
   },
-  mac(params){
-    return getOrSet('mac', params);
+  mac(params) {
+    return getOrSet('mac', params)
   },
-  operator(params){
-    return getOrSet('operator', params);
+  operator(params) {
+    return getOrSet('operator', params)
   },
-  platform(params){
-    return getOrSet('platform', params);
+  platform(params) {
+    return getOrSet('platform', params)
   },
-  packages(params){
-    return getOrSet('packages', params);
+  packages(params) {
+    return getOrSet('packages', params)
   },
-  uid(params){
-    return getOrSet('uid', params);
+  uid(params) {
+    return getOrSet('uid', params)
   },
-  stbType(params){
-    return getOrSet('stbType', params);
-  }
-};
+  stbType(params) {
+    return getOrSet('stbType', params)
+  },
+}
