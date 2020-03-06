f6d97e4c1e521c832bbf38a6e99a4eaaf522b62a
Johan van Leest <j.van.leest@metrological.com>
2020-03-06 11:48:23 +0100

    initial commit profile defaults

---
+++ src/Profile/defaults.js
@@ -0,0 +1,27 @@
+export const initDefaults = ()=>{
+  fetch('https://jsonip.metrological.com/?maf=true')
+  .then(data=>data.json())
+  .then((data)=>{
+    defaults.city = data.geo.city ? data.geo.city : 'New York';
+    defaults.countryCode = data.geo.country ? data.geo.country : 'US';
+    defaults.latlon = data.geo.ll ? data.geo.ll : [40.7128, 74.0060];
+    defaults.ip = data.ip ? data.ip : '127.0.0.1';
+  });
+};
+
+export const defaults = {
+  ageRating: 'adult',
+  city: 'New York',
+  countryCode: 'US',
+  ip: '127.0.0.1',
+  household: 'b2244e9d4c04826ccd5a7b2c2a50e7d4',
+  language: 'en',
+  latlon: [40.7128, 74.0060],
+  locale: 'en-US',
+  mac: '00:00:00:00:00:00',
+  operator: 'Metrological',
+  platform: 'Metrological',
+  packages: [],
+  uid: 'ee6723b8-7ab3-462c-8d93-dbf61227998e',
+  stbType: 'Metrological'
+};
--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,55 +1,66 @@
-let getInfo = () => Promise.resolve()
-let setInfo = () => Promise.resolve()
+import Settings from '../Settings/index';
+import { initDefaults, defaults } from './defaults';
 
-export const initProfile = config => {
-  getInfo = config.getInfo
-  setInfo = config.setInfo
-}
+let getInfo = (key)=>{
+  const value = Settings.get('profile', key);
+  if(value) return value;
+  return defaults[key];
+};
 
-const getOrSet = (key, params) => (params ? setInfo(key, params) : getInfo(key))
+let setInfo = (key, params)=>{
+  if(key in defaults) defaults[key] = params;
+};
+
+export const initProfile = config=>{
+  getInfo = config.getInfo;
+  setInfo = config.setInfo;
+  initDefaults();
+};
+
+const getOrSet = (key, params)=>(params ? setInfo(key, params) : getInfo(key));
 
 // public API
 export default {
-  ageRating(params) {
-    return getOrSet('ageRating', params)
+  ageRating(params){
+    return getOrSet('ageRating', params);
   },
-  city(params) {
-    return getOrSet('city', params)
+  city(params){
+    return getOrSet('city', params);
   },
-  countryCode(params) {
-    return getOrSet('countryCode', params)
+  countryCode(params){
+    return getOrSet('countryCode', params);
   },
-  ip(params) {
-    return getOrSet('ip', params)
+  ip(params){
+    return getOrSet('ip', params);
   },
-  household(params) {
-    return getOrSet('household', params)
+  household(params){
+    return getOrSet('household', params);
   },
-  language(params) {
-    return getOrSet('language', params)
+  language(params){
+    return getOrSet('language', params);
   },
-  latlon(params) {
-    return getOrSet('latlon', params)
+  latlon(params){
+    return getOrSet('latlon', params);
   },
-  locale(params) {
-    return getOrSet('locale', params)
+  locale(params){
+    return getOrSet('locale', params);
   },
-  mac(params) {
-    return getOrSet('mac', params)
+  mac(params){
+    return getOrSet('mac', params);
   },
-  operator(params) {
-    return getOrSet('operator', params)
+  operator(params){
+    return getOrSet('operator', params);
   },
-  platform(params) {
-    return getOrSet('platform', params)
+  platform(params){
+    return getOrSet('platform', params);
   },
-  packages(params) {
-    return getOrSet('packages', params)
+  packages(params){
+    return getOrSet('packages', params);
   },
-  uid(params) {
-    return getOrSet('uid', params)
+  uid(params){
+    return getOrSet('uid', params);
   },
-  stbType(params) {
-    return getOrSet('stbType', params)
-  },
-}
+  stbType(params){
+    return getOrSet('stbType', params);
+  }
+};
