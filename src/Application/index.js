e41454557409fb620b37d3654e41f060b60abd60
wouterlucas <wouter@wouterlucas.com>
2020-03-18 20:04:21 -0700

    App: Metric and closure changes
    Split out closeApp and the cleanup, this allows the bootstrapper to call
    close externally.
    Add this.destroy() to trigger lightning cleanup, so we do some GC before
    exiting.
    Move launch/close metric to bootstrapper.

--- src/Application/index.js
+++ src/Application/index.js
@@ -33,7 +33,6 @@ if (window.innerHeight === 720) {
 }
 
 export default function(App, appData, platformSettings) {
-  Metrics.app.launch()
   return class Application extends Lightning.Application {
     constructor(options) {
       const config = Deepmerge(defaultOptions, options)
@@ -86,15 +85,22 @@ export default function(App, appData, platformSettings) {
     }
 
     closeApp() {
-      Log.info('Closing App')
       if (platformSettings.onClose && typeof platformSettings.onClose === 'function') {
-        Metrics.app.close()
         platformSettings.onClose()
+      } else {
+        this.close()
       }
+    }
+
+    close() {
+      Log.info('Closing App')
+      Metrics.app.close()
+
       this.childList.remove(this.tag('App'))
 
       // force texture garbage collect
       this.stage.gc()
+      this.destroy()
     }
 
     loadFonts(fonts) {
