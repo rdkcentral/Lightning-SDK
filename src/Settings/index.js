4dfac7a1628686aab9f1c79a388fb2477193f902
Chris Lorenzo <Christopher_Lorenzo@Comcast.com>
2020-04-28 10:50:29 -0400

    Add setter and subscribe to Settings

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,8 +1,13 @@
 const settings = {}
+let subscribers = []
 
 export const initSettings = (appSettings, platformSettings) => {
   settings['app'] = appSettings
   settings['platform'] = platformSettings
+}
+
+function publish(type, key, value) {
+  subscribers.forEach(subscriber => subscriber(type, key, value))
 }
 
 // todo: support key for nested settings with dot notation? e.g. stage.useImageworker from 'app' settings
@@ -12,5 +17,16 @@ export default {
   },
   has(type, key) {
     return settings[type] && key in settings[type]
+  },
+  set(key, value, type = 'user') {
+    settings[type] = settings[type] || {}
+    settings[type][key] = value
+    publish(type, key, value)
+  },
+  subscribe(callback) {
+    subscribers.push(callback)
+  },
+  clearSubscribers() {
+    subscribers = []
   },
 }
