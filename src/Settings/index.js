988c41ffc394b0aac64e01453ce3cf0564240122
Michiel van der Geest <hello@michielvandergeest.com>
2020-05-27 21:08:13 -0300

    Added some improvements to the settings subscribe functionality.

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -8,7 +8,7 @@ export const initSettings = (appSettings, platformSettings) => {
 }
 
 const publish = (key, value) => {
-  subscribers[key].forEach(subscriber => subscriber(value))
+  subscribers[key] && subscribers[key].forEach(subscriber => subscriber(value))
 }
 
 // todo: support key for nested settings with dot notation? e.g. stage.useImageworker from 'app' settings
@@ -20,15 +20,26 @@ export default {
     return settings[type] && key in settings[type]
   },
   set(key, value) {
-    const type = 'user'
-    settings[type][key] = value
+    settings['user'][key] = value
     publish(key, value)
   },
   subscribe(key, callback) {
     subscribers[key] = subscribers[key] || []
     subscribers[key].push(callback)
   },
-  clearSubscribers(key) {
-    subscribers[key] = []
+  unsubscribe(key, callback) {
+    if (callback) {
+      const index = subscribers[key] && subscribers[key].findIndex(cb => cb === callback)
+      index > -1 && subscribers[key].splice(index, 1)
+    } else {
+      if (key in subscribers) {
+        subscribers[key] = []
+      }
+    }
+  },
+  clearSubscribers() {
+    for (const key of Object.getOwnPropertyNames(subscribers)) {
+      delete subscribers[key]
+    }
   },
 }
