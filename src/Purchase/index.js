683431ab7069161d0ceda7452b95b23053ee82bf
Thom van Ledden <tvledden@gmail.com>
2020-06-05 15:55:28 +0200

    Add option to force errors

--- src/Purchase/index.js
+++ src/Purchase/index.js
@@ -3,6 +3,52 @@ import Settings from '../Settings'
 
 let doPurchase = paymentObj => {
   let obj
+  const debugMapping = Settings.get('app', 'purchaseErrorMapping')
+  const debugCodes = [
+    {
+      code: 4000,
+      message: 'Missing Parameters', // One or more parameters where missing from the request.
+    },
+    {
+      code: 4001,
+      message: 'Currency Unknown', // An unknown currency was used.
+    },
+    {
+      code: 4002,
+      message: 'Household Non-existent', // The household ID cannot be found in the system.
+    },
+    {
+      code: 4003,
+      message: 'Operator Non-existent', // The operator cannot be found in the system.
+    },
+    {
+      code: 4004,
+      message: 'Invalid Signature', // The signature used is not valid.
+    },
+    {
+      code: 4005,
+      message: 'App Already Owned', // The user already owns this app.
+    },
+    {
+      code: 4010,
+      message: 'Price Not Accepted', // The price was not accepted.
+    },
+    {
+      code: 4011,
+      message: 'Credit Limit Reached', // The user has no more credit to complete the transaction.
+    },
+    {
+      code: 5000,
+      message: 'Unexpected Error', // Something has gone wrong at Metrological.
+    },
+    {
+      code: 5001,
+      message: 'Unexpected Error', // Something has gone wrong at the operator.
+    },
+  ]
+  if (debugMapping && debugMapping[paymentObj.id])
+    throw new Error(debugCodes.find(x => x.code === debugMapping[paymentObj.id]))
+
   return Promise.all([
     Profile.household(),
     Profile.countryCode(),
@@ -31,9 +77,9 @@ let doPurchase = paymentObj => {
         body: obj,
       })
     })
+    .then(response => response.json())
     .then(response => {
-      console.log(response)
-      if (!response.ok) throw new Error(`${response.status}: ${response.statusText}`)
+      if (response.code) throw response
       return response.json()
     })
 }
