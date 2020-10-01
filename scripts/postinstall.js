8f8e436255106f799215ea35d759dd6776c8671d
Michiel van der Geest <hello@michielvandergeest.com>
2020-10-01 18:42:15 -0300

    Added packagename replacement to postinstall script.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -20,14 +20,16 @@
 const shell = require('shelljs')
 const path = require('path')
 const process = require('process')
+const replaceInFile = require('replace-in-file')
 
 const nodeModulesFolder = path.join(
   process.cwd(),
   process.cwd().indexOf('node_modules') > -1 ? '../..' : 'node_modules'
 )
 
-// create support lib (should already exist)
+// create support lib (remove it first if it exists)
 const supportFolder = path.join(process.cwd(), '/support')
+shell.rm('-rf', supportFolder)
 shell.mkdir(supportFolder)
 
 // create support/lib and copy all required libraries from node_modules
@@ -50,3 +52,36 @@ shell.cp(
   path.join(nodeModulesFolder + '/@babel/polyfill/dist/polyfill.js'),
   path.join(supportPolyfillsFolder, '/babel-polyfill.js')
 )
+
+replaceInFile({
+  files: process.env.INIT_CWD + '/src/**/*',
+  // eslint-disable-next-line
+  from: /(?:[^\/]*?)\s+from\s+(["'])(wpe-lightning-sdk)(["']);?/gi,
+  to: match => {
+    return match.replace('wpe-lightning-sdk', '@lightningjs/sdk')
+  },
+})
+  .then(result => {
+    const changedFiles = result
+      .filter(item => item.hasChanged === true)
+      .map(item => '- ' + item.file.replace(process.env.INIT_CWD, ''))
+
+    if (changedFiles.length) {
+      console.log('\x1b[32m')
+      console.log('=============================================================================\n')
+      console.log(
+        'The package name of the Lightning SDK has changed from "wpe-lightning-sdk"\nto "@lightningjs/sdk"'
+      )
+      console.log('\n\nFrom now on you should now import plugins from the Lightning SDK like this:')
+      console.log("\n\nimport { Utils } from '@lightningjs/sdk'\n")
+      console.log('=============================================================================')
+
+      console.log('\n\nThe following files have been automatically updated for you:\n\n')
+      console.log('\x1b[0m')
+      console.log(changedFiles.join('\n'))
+      console.log('\n\n')
+    }
+  })
+  .catch(e => {
+    console.error(e)
+  })
