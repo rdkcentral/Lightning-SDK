87a21876101b7ce81c45ef0800671b341c19e455
Michiel van der Geest <hello@michielvandergeest.com>
2020-11-09 11:31:35 -0300

    Added current time as argument in VideoPlayer events. Added 'Clear'-event.

--- src/VideoPlayer/index.js
+++ src/VideoPlayer/index.js
@@ -83,8 +83,8 @@ const withPrecision = val => Math.round(precision * val) + 'px'
 
 const fireOnConsumer = (event, args) => {
   if (consumer) {
-    consumer.fire('$videoPlayer' + event, args)
-    consumer.fire('$videoPlayerEvent', event, args)
+    consumer.fire('$videoPlayer' + event, args, videoEl.currentTime)
+    consumer.fire('$videoPlayerEvent', event, args, videoEl.currentTime)
   }
 }
 
@@ -257,6 +257,7 @@ const videoPlayerPlugin = {
     // pause the video first to disable sound
     this.pause()
     if (textureMode === true) videoTexture.stop()
+    fireOnConsumer('Clear', { videoElement: videoEl })
     videoEl.removeAttribute('src')
     videoEl.load()
   },
