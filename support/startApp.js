dc17deac49f785b930fcc214d21f413476994491
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2021-02-18 23:49:35 +0530

    Updated startApp.js in support folder

--- support/startApp.js
+++ support/startApp.js
@@ -1,5 +1,54 @@
 'use strict';
 
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
+function ownKeys(object, enumerableOnly) {
+  var keys = Object.keys(object);
+
+  if (Object.getOwnPropertySymbols) {
+    var symbols = Object.getOwnPropertySymbols(object);
+    if (enumerableOnly) symbols = symbols.filter(function (sym) {
+      return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+    });
+    keys.push.apply(keys, symbols);
+  }
+
+  return keys;
+}
+
+function _objectSpread2(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i] != null ? arguments[i] : {};
+
+    if (i % 2) {
+      ownKeys(Object(source), true).forEach(function (key) {
+        _defineProperty(target, key, source[key]);
+      });
+    } else if (Object.getOwnPropertyDescriptors) {
+      Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+    } else {
+      ownKeys(Object(source)).forEach(function (key) {
+        Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+      });
+    }
+  }
+
+  return target;
+}
+
 function _newArrowCheck(innerThis, boundThis) {
   if (innerThis !== boundThis) {
     throw new TypeError("Cannot instantiate an arrow function");
@@ -99,15 +148,19 @@ var startApp = function startApp() {
   }.bind(this), function () {
     _newArrowCheck(this, _this2);
 
-    var bundle = window[appMetadata.id]; // support rollup and esbuild
+    var bundle = window[appMetadata.safeId]; // support rollup and esbuild
 
     if (typeof bundle !== 'function') {
       bundle = bundle.default;
     }
 
-    console.time('app2');
-    settings.appSettings.version = appMetadata.version;
-    settings.appSettings.id = appMetadata.identifier;
+    console.time('app2'); //Adding complete metadata info to app settings
+
+    settings.appSettings = _objectSpread2({}, appMetadata); //To align with the production response, adding the 'identifier' as 'id'
+
+    settings.appSettings.id = appMetadata.identifier; //Deleting the identifier as it is no longer required
+
+    delete settings.appSettings.identifier;
     app = bundle(settings.appSettings, settings.platformSettings, settings.appData);
     canvas = app.stage.getCanvas();
     document.body.appendChild(canvas);
@@ -122,7 +175,7 @@ var getAppMetadata = function getAppMetadata() {
   return fetchJson('./metadata.json').then(function (metadata) {
     _newArrowCheck(this, _this7);
 
-    metadata.id = "APP_".concat(metadata.identifier.replace(/[^0-9a-zA-Z_$]/g, '_'));
+    metadata.safeId = "APP_".concat(metadata.identifier.replace(/[^0-9a-zA-Z_$]/g, '_'));
     return metadata;
   }.bind(this));
 }.bind(undefined);
