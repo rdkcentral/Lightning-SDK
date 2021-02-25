d3962e502d7921f52b2971df47872dc5a43c4a1d
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2021-02-25 14:28:55 +0530

    Updated startapp js in support folder

--- support/startApp.js
+++ support/startApp.js
@@ -1,54 +1,5 @@
 'use strict';
 
-function _defineProperty(obj, key, value) {
-  if (key in obj) {
-    Object.defineProperty(obj, key, {
-      value: value,
-      enumerable: true,
-      configurable: true,
-      writable: true
-    });
-  } else {
-    obj[key] = value;
-  }
-
-  return obj;
-}
-
-function ownKeys(object, enumerableOnly) {
-  var keys = Object.keys(object);
-
-  if (Object.getOwnPropertySymbols) {
-    var symbols = Object.getOwnPropertySymbols(object);
-    if (enumerableOnly) symbols = symbols.filter(function (sym) {
-      return Object.getOwnPropertyDescriptor(object, sym).enumerable;
-    });
-    keys.push.apply(keys, symbols);
-  }
-
-  return keys;
-}
-
-function _objectSpread2(target) {
-  for (var i = 1; i < arguments.length; i++) {
-    var source = arguments[i] != null ? arguments[i] : {};
-
-    if (i % 2) {
-      ownKeys(Object(source), true).forEach(function (key) {
-        _defineProperty(target, key, source[key]);
-      });
-    } else if (Object.getOwnPropertyDescriptors) {
-      Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
-    } else {
-      ownKeys(Object(source)).forEach(function (key) {
-        Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
-      });
-    }
-  }
-
-  return target;
-}
-
 function _newArrowCheck(innerThis, boundThis) {
   if (innerThis !== boundThis) {
     throw new TypeError("Cannot instantiate an arrow function");
@@ -156,7 +107,7 @@ var startApp = function startApp() {
 
     console.time('app2'); //Adding complete metadata info to app settings
 
-    settings.appSettings = _objectSpread2({}, appMetadata); //To align with the production response, adding the 'identifier' as 'id'
+    Object.assign(settings.appSettings, appMetadata); //To align with the production response, adding the 'identifier' as 'id'
 
     settings.appSettings.id = appMetadata.identifier; //Deleting the identifier as it is no longer required
 
